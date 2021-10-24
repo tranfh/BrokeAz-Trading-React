0bc82e28621114cd2c5858bc6659dd981a73f81c
Frank Tran <frank.tran@live.ca>
2021-10-23 22:44:20 -0400

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Movers from './components/Movers/Movers';
 import Futures from './components/Futures/Futures';
@@ -8,23 +8,17 @@ import Trending from './components/Trending/Trending';
 import Actives from './components/Actives/Actives';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="App bg-dark">
-        <Navigation />
-        <Futures />
-        <Actives />
-        <Movers />
-        <Trending />
-        <News />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App bg-dark">
+      <Navigation />
+      <Futures />
+      <Actives />
+      <Movers />
+      <Trending />
+      <News />
+    </div>
+  );
+};
 
 export default App;
